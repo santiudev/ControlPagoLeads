6f180ee57e9e58d2284a7e06b0c5ba4774fdbbe6
Santiago Urban <urbansanty@gmail.com>
2023-04-04 11:14:28 -0300

    2do

--- base/base/urls.py
+++ base/base/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView 
+from leads.views import AssignLeadView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('closer.urls')),  
     path('', RedirectView.as_view(url='login/')),  
     path('leads/', include('leads.urls')),
+    path('assign_lead/', AssignLeadView.as_view(), name='assign_lead'),
+    
 ]
